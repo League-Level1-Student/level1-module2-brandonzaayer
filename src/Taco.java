92e6e8e9c89c20d650745970c2824b3c79dbea1e
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-01-16 17:00:43 -0800

    I got distracted and didn't finish my taco class

---
+++ src/Taco.java
@@ -0,0 +1,17 @@
+
+ class Taco {
+	private String sauce;
+	private String meat;
+	 String getMeat(){
+		 return meat;
+	 }
+	 String getSauce() {
+		 return sauce;
+	 }
+	void  setMeat(String meat){
+		 
+	 }
+	 void setSauce(String sauce){
+		 
+	 }
+}
