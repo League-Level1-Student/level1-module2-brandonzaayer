8da25b08c145ff7421871da143bf5dd1b3a5fe4f
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-02-27 16:59:55 -0800

    A finished tea party

--- src/TeaParty.java
+++ src/TeaParty.java
@@ -4,8 +4,18 @@ public class TeaParty {
  public String welcome(String name, boolean isWoman, boolean isKnighted)
  {
 	if(isKnighted==true && isWoman==false) {
-		
+		return "Hello Sir "+ name;
 	}
-
+	else if (isKnighted==false && isWoman==false) {
+		return "Hello Mr. " + name;
+	}
+	else if (isKnighted==true && isWoman==true) {
+		return "Hello Lady " + name;
+	}
+	else {
+		return "Hello Ms. " + name;
+	}
+	
+ }
 }
-}
+	
